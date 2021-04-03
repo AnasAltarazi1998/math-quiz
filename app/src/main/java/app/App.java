1d7f0747da7005e828d6807429a2efc35a4a10b1
anas_altarazi <anas.anas1998,tar@gmail.com>
2021-04-03 14:50:19 +0300

    -- final aupdate

--- app/src/main/java/app/App.java
+++ app/src/main/java/app/App.java
@@ -1,53 +1,24 @@
 package app;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-
+import java.util.Scanner;
 public class App 
 {
     public static void main( String[] args )
     {  
-        int c = 0;
-        String res = "";
-        File f = new File("encoded.txt");
-        try {
-            FileReader fr = new FileReader(f);
-            FileWriter fw = new FileWriter(f, true);
-            fw.write(" value");
-            fw.flush();
-            testBuffers();
-            while((c = fr.read()) != -1)
-            {
-                res+=(char)c;
-            }
-            System.out.print(res);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+     
+            System.out.println("1- resgister \n2- sign in \n3- info");
+            Scanner in = new Scanner(System.in);
+            int select = in.nextInt();
+            switch (select) {
+                case 1:
+                    System.out.print("reg process .....");
+                    break;
+                case 2:
+                    break;
+                default:
+
+                    break;
+        
         }
-
     }
-
-    public static void testBuffers() throws IOException
-    {
-        File f = new File("encoded.txt");
-        BufferedWriter bw = new BufferedWriter(new FileWriter(f));
-        bw.write("bufferedwriter is writing");
-        bw.flush();
-        bw.close();
-
-        BufferedReader br = new BufferedReader(new FileReader(f));
-        System.out.println(br.readLine());
-        br.close();
-    }
-
 }
